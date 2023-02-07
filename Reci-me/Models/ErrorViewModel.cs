f2dc270a1d8545850d4d26109856e64df7274f6d
Max <maxwell.wilke@yahoo.com>
2023-02-06 18:37:16 -0600

    Add project files.

---
+++ Reci-me/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Reci_me.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
