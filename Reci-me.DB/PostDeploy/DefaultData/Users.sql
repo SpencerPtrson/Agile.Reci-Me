775d7bb10c6da495c98f0b6d4a5404d932cf9bfb
Spencer Peterson <700230728@fvtc.edu>
2023-02-10 15:08:20 -0600

    Added User default data

--- Reci-me.DB/PostDeploy/DefaultData/Users.sql
+++ Reci-me.DB/PostDeploy/DefaultData/Users.sql
@@ -1,1 +1,8 @@
-﻿
+﻿BEGIN
+	INSERT INTO tblUser VALUES
+		(NEWID(),'spencer.peterson0728@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
+		(NEWID(),'byron.baker5104@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
+		(NEWID(),'abigail.proudlock0180@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
+		(NEWID(),'maxwell.wilke3555@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
+		(NEWID(),'john.yang9556@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin'))
+END
