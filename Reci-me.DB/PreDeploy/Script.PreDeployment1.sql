8f0a881195ce4ec1e2dbfa247543d3b3d180e2eb
Spencer Peterson <700230728@fvtc.edu>
2023-02-10 13:44:57 -0600

    Added DefaultData, PreDeploy and PostDeploy Script files

---
+++ Reci-me.DB/PreDeploy/Script.PreDeployment1.sql
@@ -0,0 +1,17 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+DROP TABLE IF EXISTS dbo.tblRecipeIngredient
+DROP TABLE IF EXISTS dbo.tblRecipeImage
+DROP TABLE IF EXISTS dbo.tblIngredient
+DROP TABLE IF EXISTS dbo.tblRecipe
+DROP TABLE IF EXISTS dbo.tblAccessLevel
