5802b17cf3105a500cf15d5be4ad6060e81377fa
Max <maxwell.wilke@yahoo.com>
2023-02-09 21:16:21 -0600

    Added Ingredient and RecipeImage classes

---
+++ Reci-me.BL.Models/RecipeImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reci_me.BL.Models
+{
+    public class RecipeImage
+    {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public bool IsThumbnail { get; set; }
+    }
+}
