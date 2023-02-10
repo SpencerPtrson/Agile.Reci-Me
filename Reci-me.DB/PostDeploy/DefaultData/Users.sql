d9c1643b3fae268d926d79b773e86d94dd3c0215
Spencer Peterson <700230728@fvtc.edu>
2023-02-10 15:49:40 -0600

    Changed function names and added function call to User default data

--- Reci-me.DB/PostDeploy/DefaultData/Users.sql
+++ Reci-me.DB/PostDeploy/DefaultData/Users.sql
@@ -1,8 +1,9 @@
-﻿BEGIN
+﻿
+BEGIN
 	INSERT INTO tblUser VALUES
-		(NEWID(),'spencer.peterson0728@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
-		(NEWID(),'byron.baker5104@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
-		(NEWID(),'abigail.proudlock0180@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
-		(NEWID(),'maxwell.wilke3555@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin')),
-		(NEWID(),'john.yang9556@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', AccessId('Admin'))
+		(NEWID(),'spencer.peterson0728@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', dbo.fnAccessId('Admin')),
+		(NEWID(),'byron.baker5104@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', dbo.fnAccessId('Admin')),
+		(NEWID(),'abigail.proudlock0180@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', dbo.fnAccessId('Admin')),
+		(NEWID(),'maxwell.wilke3555@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', dbo.fnAccessId('Admin')),
+		(NEWID(),'john.yang9556@fvtc.edu', 'Agile', 'imagePath', 'ProfileDescription', dbo.fnAccessId('Admin'))
 END
