b7caea37fe994feb450c4bc2a872ae0b01cb01c9
Spencer Peterson <700230728@fvtc.edu>
2023-02-19 18:13:23 -0600

    Apply Staged

--- Reci-me.DB/PostDeploy/DefaultData/Recipes.sql
+++ Reci-me.DB/PostDeploy/DefaultData/Recipes.sql
@@ -1,1 +1,14 @@
-﻿
+﻿BEGIN
+	INSERT INTO tblRecipe VALUES
+		(NEWID(), 'Blackened Shrimp Tacos', 4, 15.00, 
+		31.00, dbo.fnUserID('spencer.peterson0728@fvtc.edu'), 0, 'Shrimp Taco Image Path'),
+		
+		(NEWID(), 'Steak on the Stovetop', 2, 30.00, 
+		40.00, dbo.fnUserID('spencer.peterson0728@fvtc.edu'), 0, 'Steak Image Path'),
+		
+		(NEWID(), 'Stuffed Peppers', 6, 10.00, 
+		80.00, dbo.fnUserID('spencer.peterson0728@fvtc.edu'), 0, 'Stuffed Pepper Image Path'),
+		
+		(NEWID(), 'Pasta Fazool', 2, 10.00, 
+		35.00, dbo.fnUserID('spencer.peterson0728@fvtc.edu'), 0, 'Pasta Fazool Image Path')	
+END
