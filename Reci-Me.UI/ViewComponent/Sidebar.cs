a98daf561d793ba359ece42a0a3f82107ba86cb1
Spencer Peterson <700230728@fvtc.edu>
2023-02-28 20:24:37 -0600

    Added Sidebar view component back in

---
+++ Reci-Me.UI/ViewComponent/Sidebar.cs
@@ -0,0 +1,13 @@
+﻿using Reci_me.BL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reci_Me.UI.ViewComponents
+{
+    public class Sidebar : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(CategoryManager.Load().OrderBy(c => c.Name));
+        }
+    }
+}
