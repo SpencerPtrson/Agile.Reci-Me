bb611fa6cd106e3c576503120c2d5dbd28786e1e
Spencer Peterson <700230728@fvtc.edu>
2023-03-24 19:53:44 -0500

    Got a start on Add Recipe page Added ViewModel folder and RecipeVM

---
+++ Reci-Me.UI/ViewModels/RecipeVM.cs
@@ -0,0 +1,22 @@
+﻿using Reci_me.BL;
+using Reci_me.BL.Models;
+
+namespace Reci_Me.UI.ViewModels
+{
+    public class RecipeVM
+    {
+        public Recipe Recipe { get; set; }
+        public List<Category> Categories { get; set; }
+
+        // Constructor used with the word Create
+        public RecipeVM() {
+            Categories = CategoryManager.Load();
+        }
+
+        // Constructor used with the word Edit
+        public RecipeVM(int id)
+        {
+            Categories = CategoryManager.Load();
+        }
+    }
+}
