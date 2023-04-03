a57a6746d88e16759a30a03a9ae46f416b371ff3
Max <maxwell.wilke@yahoo.com>
2023-04-03 13:41:27 -0500

    Added weekly recipe functunality

---
+++ Reci-Me.UI/Controllers/ViewRecipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reci_Me.UI.Controllers
+{
+    public class ViewRecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
