1ca810f3ca8c035daab0f0ad39b8abbb9e266f6d
Spencer Peterson <700230728@fvtc.edu>
2023-04-08 10:33:51 -0500

    Added RecipeController with a create and index page

---
+++ Reci-Me.UI/Controllers/RecipeController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reci_me.BL;
+using Reci_Me.UI.ViewModels;
+
+namespace Reci_Me.UI.Controllers
+{
+    public class RecipeController : Controller
+    {
+
+        // LOADS
+        // GET: RecipeController
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Recipes";
+            return View(RecipeManager.Load());
+        }
+
+        // GET: RecipeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Browse(Guid id)
+        {
+            ViewBag.Title = "Recipes";
+            return View(nameof(Index), RecipeManager.Load(id));
+        }
+
+
+        // CREATES
+        // GET: RecipeController/Create
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Create a Recipe";
+            RecipeVM recipeVM = new RecipeVM();
+            return View(recipeVM);
+        }
+
+        // POST: SubmitRecipeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(RecipeVM recipeVM)
+        {
+            try
+            {
+                RecipeManager.Insert(recipeVM.Recipe);
+                return RedirectToAction(nameof(Index), "Home");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                ViewBag.Title = "Create a Recipe";
+                return View(recipeVM);
+            }
+        }
+
+
+        // EDITS
+        // GET: RecipeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RecipeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // DELETES
+        // GET: RecipeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RecipeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
